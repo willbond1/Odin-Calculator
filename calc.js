f1c8a6a80b0fa06d7e68b258591a5aff398bedb6
Will <willbond1@gmail.com>
2023-05-07 17:58:14 -0400

    Finished calculator

--- calc.js
+++ calc.js
@@ -1,4 +1,14 @@
 
+const display = document.querySelector("#display");
+const numBtns = document.querySelectorAll("button.num");
+const opBtns = document.querySelectorAll("button.op");
+const clrBtn = document.querySelector("button#clr");
+const eqlBtn = document.querySelector("button#eql");
+
+let numBuf = "";
+let firstNum = NaN;
+let op = "";
+
 const add = (x, y) => x + y;
 const sub = (x, y) => x - y;
 const mul = (x, y) => x * y;
@@ -15,9 +25,48 @@ function operate(op, x, y) {
             return mul(x, y);
         case "/":
             return div(x, y);
-        case "%":
-            return mod(x, y);
         default:
             return undefined;
     }
 }
+
+numBtns.forEach(btn => btn.addEventListener("click", function() {
+    const lastChar = this.textContent;
+    numBuf += lastChar;
+    display.textContent += lastChar;
+}));
+
+opBtns.forEach(btn => btn.addEventListener("click", function() {
+    const opChar = this.textContent;
+    if(isNaN(firstNum)) { // this is the first operand
+        op = opChar;
+        firstNum = Number(numBuf);
+        display.textContent += opChar;
+    } else {
+        const secondNum = Number(numBuf);
+        const result = operate(op, firstNum, secondNum);
+        firstNum = result;
+        op = opChar;
+        display.textContent = result;
+        display.textContent += op;
+    }
+    numBuf = "";
+}));
+
+eqlBtn.addEventListener("click", function() {
+    if(!isNaN(firstNum) && op !== "" && numBuf !== "") {
+        const secondNum = Number(numBuf);
+        const result = operate(op, firstNum, secondNum);
+        display.textContent = result;
+        numBuf = String(result);
+        op = "";
+        firstNum = NaN;
+    }
+});
+
+clrBtn.addEventListener("click", function() {
+    display.textContent = "";
+    numBuf = "";
+    op = "";
+    firstNum = NaN;
+});
